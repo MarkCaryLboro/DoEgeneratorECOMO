150cbc7aa469f762604b280d153998a48ab17230
MarkCaryLboro <64597423+MarkCaryLboro@users.noreply.github.com>
2023-01-19 13:57:32 +0000

    added generation of B-spline array
    Add bSplineTools() array generation for Bspline property. Automatically creates an array of Bspline objects for each. Each element is associated with a distributed factor

--- DoEgeneratorECOMO.m
+++ DoEgeneratorECOMO.m
@@ -17,7 +17,7 @@ classdef DoEgeneratorECOMO < handle
     properties ( SetAccess = protected )
         Factors     (:,:)    table                                          % Factor details and type
         Scramble    (1,1)    logical = false                                % Set to true to apply scramble to design
-        Design      (:,:)    double                                         % Design table
+        Design      (:,:)    double                                         % Design array
     end % SetAccess protected
 
     properties ( Access = private )
@@ -26,6 +26,9 @@ classdef DoEgeneratorECOMO < handle
 
     properties ( SetAccess = protected, Dependent = true )
         NumPoints           int64                                           % Number of points in the design
+        NumFactors          int64                                           % Number of factors
+        NumFixed            int64                                           % Number of fixed factors
+        NumDist             int64                                           % Number of B-spline factors
     end % Accessible dependent properties
 
     methods ( Abstract = true )
@@ -34,7 +37,18 @@ classdef DoEgeneratorECOMO < handle
     end % abstract method signatures
 
     methods
-        function obj = addFactor( obj, S )
+        function obj = clearDesign( obj )
+            %--------------------------------------------------------------
+            % Set the design array to empty and reset Scramble property to
+            % false
+            %
+            % obj = obj.clearDesign();
+            %--------------------------------------------------------------
+            obj.Design = [];
+            obj.Scramble = false;
+        end % clearDesign
+
+        function obj = addFactor( obj, S, Opts )
             %--------------------------------------------------------------
             % Add a factor to the factor table property. If factor is
             % already defined, details are overwritten with the new
@@ -59,6 +73,8 @@ classdef DoEgeneratorECOMO < handle
             arguments
                 obj     (1,1)   
                 S       (1,:)   struct   { mustBeNonempty( S ) }
+                Opts.M  (1,1)   int8 = 4
+                Opts.K  (1,1)   int8 = 2
             end
             Q = max( size( S ) );
             %--------------------------------------------------------------
@@ -67,6 +83,10 @@ classdef DoEgeneratorECOMO < handle
             Name = [ S.Name ];
             Ok = ( numel( unique( Name ) ) == Q );
             assert( Ok, "Factor names may not be repeated!");
+            %--------------------------------------------------------------
+            % Clear the design
+            %--------------------------------------------------------------
+            obj = obj.clearDesign();
             %--------------------------------------------------------------
             % Check all necessary fields are present & parse
             %--------------------------------------------------------------
@@ -83,10 +103,48 @@ classdef DoEgeneratorECOMO < handle
             else
                 obj.Factors = T;
             end
+            %--------------------------------------------------------------
+            % Create B-spline array for distributed factors
+            %--------------------------------------------------------------
+            obj = obj.createBsplineArray( Opts.M, Opts.K );
         end % addFactor
     end % ordinary methods
 
     methods ( Access = protected )
+        function obj = createBsplineArray( obj, M, K )
+            %--------------------------------------------------------------
+            % Create a B-spline representation for each distributed
+            % parameter
+            %
+            % obj = obj.createBsplineArray( M, K );
+            %
+            % Input Arguments:
+            %
+            % M     --> (int8) Spline order (1 <= M <= 4)
+            % K     --> (int8) Number of knots (1 <= K <= 7)
+            %--------------------------------------------------------------
+            arguments
+                obj (1,1)                   { mustBeNonempty( obj ) }
+                M   (1,1) int8  { mustBeGreaterThan(M,0), ...
+                                  mustBeLessThan(M,5)} = 4;
+                K   (1,1) int8  { mustBeGreaterThan(K,0), ...
+                                  mustBeLessThan(K,8)} = 2;
+            end
+            D = ~obj.Factors{ :, "Fixed" };
+            N = sum( D );
+            if ( N > 0 )
+                obj.Bspline( N ) = bSplineTools.empty;
+                Lo = obj.Factors{ :, "Lo" }( D );
+                Hi = obj.Factors{ :, "Hi" }( D );
+                for Q = 1:N
+                    DK = linspace( Lo( Q ), Hi( Q ), K + 2 ).';
+                    DK = DK( 2:end-1 );
+                    obj.Bspline( Q ) = bSplineTools( ( M - 1 ), DK,...
+                        Lo( Q ), Hi( Q ) );
+                end
+            end
+        end % createBsplineArray
+
         function T = parseFactor( obj, S )
             %--------------------------------------------------------------
             % A method to parse the factor definition structure
@@ -142,6 +200,18 @@ classdef DoEgeneratorECOMO < handle
     methods
         function N = get.NumPoints( obj )
             N = int64( obj.NumPoints_ );                                    % Number of points in the design
+        end
+
+        function N = get.NumFactors( obj )                             
+            N = height( obj.Factors );                                      % Number of factors
+        end
+
+        function N = get.NumFixed( obj )                                           
+            N = sum( obj.Factors{ :, "Fixed" } );                           % Number of fixed factors
+        end
+
+        function N = get.NumDist( obj )
+            N = sum( ~obj.Factors{ :, "Fixed" } );                          % Number of B-spline factors
         end
     end % set/get methods
 
--- SobolSequence.m
+++ SobolSequence.m
@@ -27,7 +27,22 @@ classdef SobolSequence < DoEgeneratorECOMO
     end % Constructor method
 
     methods 
-        function obj = generate( obj, varargin )
+        function obj = generate( obj, Opts )
+            %--------------------------------------------------------------
+            % Generate a design
+            %
+            % obj = obj.generate( "Name1", "value1", ..., "Name#", value# )
+            %--------------------------------------------------------------
+            arguments
+                obj         (1,1)   SobolSequence
+                Opts.Size   (1,1)   double         = 101
+                Opts.Leap   (1,1)   double         = 7301
+                Opts.Skip   (1,1)   double         = 49
+            end
+            D = height( obj.Factors );
+            P = sobolset( D, "Leap", Opts.Leap, "Skip", Opts.Skip );
+            Des = net( P, Opts.Size );
+
         end % generate
         
         function obj = scrambleDesign( obj, varargin )
